274453f390333ca2f2b66a1a69b40b65e94cb210
Sahil Dhiman <120819341+Sahil03022001@users.noreply.github.com>
2023-08-18 21:01:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-//your JS code here. If required.
+const rotatingDiv = document.getElementById('line');
+let rotation = 0;
+
+function rotateDiv() {
+    rotation += 1;
+    rotatingDiv.style.transform = `rotate(${rotation}deg)`;
+}
+
+setInterval(rotateDiv, 10);
